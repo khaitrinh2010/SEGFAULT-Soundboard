add6b60043f2110305af62cff9f890d08dfbb3b1
khai <trinhlamkhai@gmail.com>
2025-03-17 17:06:21 +0700

    add wav_save

--- sound_seg.c
+++ sound_seg.c
@@ -17,6 +17,22 @@ struct sound_seg {
     int16_t *ptr; //pointer to the buffer array
 };
 
+struct wav_header {
+     char riff[4];
+     uint32_t flength;
+     char wave[4];
+     char fmt[4];
+     int32_t chunk_size;
+     int16_t format_tag;
+     int16_t num_chans;
+     int32_t sample_rate;
+     int32_t bytes_per_sec;
+     int16_t bytes_per_sample;
+     int16_t bits_per_sample;
+     char data[4];
+     int32_t dlength;
+ }
+
 // Load a WAV file into buffer
 void wav_load(const char* filename, int16_t* dest){  //wav file header is discarded
     FILE *file;
@@ -38,6 +54,34 @@ void wav_load(const char* filename, int16_t* dest){  //wav file header is discar
 
 // Create/write a WAV file from buffer
 void wav_save(const char* fname, int16_t* src, size_t len){
+    // The songs will always be PCM, 16 bits per sample, mono, 8000Hz Sample rate
+    FILE *file;
+    file = fopen(fname, "wb");
+    if (file == NULL){
+      printf("Error opening file\n");
+      return;
+    }
+    //WRITE HEADER FIRST
+    struct wav_header header;
+    strncpy(header.riff, "RIFF", 4);
+    strncpy(header.wave, "WAVE", 4);
+    strncpy(header.fmt, "fmt ", 4);
+    strncpy(header.data, "data", 4);
+
+    header.chunk_size = 16;
+    header.format_tag = 1;
+    header.num_chans = 1;
+    header.sample_rate = 8000;
+    header.bits_per_sample = 16;
+    header.bytes_per_sample = header.bits_per_sample / 8;
+    header.bytes_per_sec = header.bytes_per_sample * header.sample_rate;
+    // len is the number of samples
+    header.dlength = len * sizeof(int16_t);
+    header.flength = header.dlength + OFFSET_TO_AUDIO_DATA;
+    fwrite(&header, sizeof(struct wav_header), 1, file);
+    fseek(file, OFFSET_TO_AUDIO_DATA, SEEK_SET);
+    fwrite(&src, sizeof(int16_t), len, file);
+    fclose(file);
     return;
 }
 
