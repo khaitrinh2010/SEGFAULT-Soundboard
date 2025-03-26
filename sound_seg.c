09968fe0f513f309990674c3ae993df54a3ede18
khai <trinhlamkhai@gmail.com>
2025-03-26 16:18:09 +0700

    a newe approach

--- sound_seg.c
+++ sound_seg.c
@@ -23,21 +23,6 @@ struct sound_seg {
     size_t total_samples;
 };
 
-struct wav_header {
-     char riff[4];
-     uint32_t flength;
-     char wave[4];
-     char fmt[4];
-     int32_t chunk_size;
-     int16_t format_tag;
-     int16_t num_chans;
-     int32_t sample_rate;
-     int32_t bytes_per_sec;
-     int16_t bytes_per_sample;
-     int16_t bits_per_sample;
-     char data[4];
-     int32_t dlength;
- };
 
 // Load a WAV file into buffer
 void wav_load(const char* filename, int16_t* dest){  //wav file header is discarded
