7f09eff143697437c791f932b335dc6c1647e3fe
khai <trinhlamkhai@gmail.com>
2025-03-27 18:58:34 +0700

    return to oold appproach

--- sound_seg.c
+++ sound_seg.c
@@ -364,31 +364,6 @@ void tr_insert(struct sound_seg* src_track, struct sound_seg* dest_track,
 }
 
 
-
 int main(int argc, char** argv) {
-    struct sound_seg* s0 = tr_init();
-    tr_write(s0, ((int16_t[]){}), 0, 0);
-    struct sound_seg* s1 = tr_init();
-    tr_write(s1, ((int16_t[]){3,18,11,-8,5,-1,-18,-15,0,-6,-5,-14,4}), 0, 13);
-    struct sound_seg* s2 = tr_init();
-    tr_write(s2, ((int16_t[]){2,19,5,13,-10,-3}), 0, 6);
-    struct sound_seg* s3 = tr_init();
-    tr_write(s3, ((int16_t[]){-9,-5,20,-12,0,-18,-1,-19,-6}), 0, 9);
-    tr_write(s3, ((int16_t[]){11,5,-2,7,-15,8,-13,-1,7}), 0, 9);
-    tr_insert(s2, s1, 9, 1, 1);
-    print_track(s1);
-    printf("\n");
-    tr_write(s2, ((int16_t[]){-20,5,12,0,11,-11}), 0, 6);
-    tr_write(s3, ((int16_t[]){-12,-18,-14,-10,5,-9,8,16,-6}), 0, 9);
-    tr_delete_range(s3, 5, 3); //expect return True
-    tr_insert(s1, s0, 0, 7, 3);
-    tr_write(s1, ((int16_t[]){-10,-6,-7,18,2,-12,12,16,-15,-13,20,-17,17,1}), 0, 14);
-    tr_write(s0, ((int16_t[]){17,-16,-11}), 0, 3);
-    int16_t FAILING_READ[14];
-    tr_read(s1, FAILING_READ, 0, 14);
-    //expected [-10  -6  -7  18   2 -12  12  17 -16 -11  20 -17  17   1], actual [-10  -6  -7  18   2 -12  12  17 -16 -13  20 -17  17   1]!
-    tr_destroy(s0);
-    tr_destroy(s1);
-    tr_destroy(s2);
-    tr_destroy(s3);
+
 }
